433daaeeb11184b4275c0b90af5ec06f0bbd60a9
Dashrendaring <119767035+Dashrendaring@users.noreply.github.com>
2022-12-05 14:00:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-let myName = document.getElementById ("nameSean");
+let myName = document.getElementById("nameSean");
 
-
-function sizeUp(item){  
-item.style.fontsize = "2.7rem";
+function sizeUp(item) {
+    item.style.fontSize = "2.6rem";
+    item.style.color = "yellow";
 }
 
-function sizeDown(item){
-    item.style.fontsize = "2.5rem";
+function sizeDown(item) {
+    item.style.fontSize = "2.5rem";
+    item.style.color = "white";
 }
 
+myName.setAttribute("onmouseenter", "sizeUp(this)");
+myName.setAttribute("onmouseleave", "sizeDown(this)");
 
- myName.setAttribute ("onmouseenter", sizeUp(this));   
- myName.setAttribute ("onmouseleave", sizeDown(this));  
- 
-//  myName.addEventListener("onmouseenter", sizeUp(this));
-//  myName.addEventListener("onmouseleave", sizeDown(this));
+
+function visitPage() {
+    window.location = 'https://github.com/Dashrendaring/Lesson10Final';
+}
+
+let myTitle = document.getElementById("title");
+
+function tUp(item) {
+    item.style.fontSize = "1.6rem";
+    item.style.color = "yellow";
+}
+
+function tDown(item) {
+    item.style.fontSize = "1.5rem";
+    item.style.color = "white";
+}
+
+myTitle.setAttribute("onmouseenter", "tUp(this)");
+myTitle.setAttribute("onmouseleave", "tDown(this)");
+
+$.ajax({
+    url: "https://api.adviceslip.com/advice",
+    type: "get",
+    dataType: "json",
+    success: function (result) {
+        console.log(result);
+
+        const postAdvice = document.getElementById("postAdvice");
+        postAdvice.innerHTML = ("", result.slip.advice);
+
+    },
+})
